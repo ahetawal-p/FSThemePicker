6629b831242ed7c5304040f564336b292b0c5ed3
Amit Hetawal <amit.hetawal@gmail.com>
2017-07-07 21:39:44 -0700

    Adding header and footer

--- App.js
+++ App.js
@@ -1,62 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View, ListView, Modal } from 'react-native';
+import { StyleSheet, Text, View, ListView, Modal, TouchableNativeFeedback } from 'react-native';
 import mockData from './mockData'
 import Row from './PickerRow'
-
-
 
 export default class App extends React.Component {
     constructor(props) {
     super(props);
 
-
-
-    const getSectionData = (dataBlob, sectionId) => dataBlob[sectionId];
-    const getRowData = (dataBlob, sectionId, rowId) => {
-                console.log(dataBlob[`${rowId}`]);
-                return dataBlob[`${rowId}`];
-            }
-
-    const ds = new ListView.DataSource({
-      rowHasChanged: (r1, r2) => r1 !== r2,
-      sectionHeaderHasChanged : (s1, s2) => s1 !== s2,
-      getSectionData,
-      getRowData,
-    });
-
-    const { dataBlob, sectionIds, rowIds } = this.formatData(mockData);
-
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.state = {
-      dataSource: ds.cloneWithRowsAndSections(dataBlob, sectionIds, rowIds),
+      dataSource: ds.cloneWithRows(mockData),
       data: mockData
     };
   }
-
-
-  formatData(data) {
-      const dataBlob = {};
-      const sectionIds = [];
-      const rowIds = [];
-      rowIds.push([]);
-
-      sectionIds.push(0);
-      dataBlob[0] = 'Header';
-
-    //   for (let sectionId = 0; sectionId < data.length; sectionId++) {
-    //       sectionIds.push(sectionId);
-    //       dataBlob[sectionId] = 'Header';
-    //   }
-      for (let rowId = 0; rowId < data.length; rowId++) {
-          const rowIdTest = `0:${rowId}`;
-         rowIds[0].push(rowIdTest);
-         dataBlob[rowIdTest] = data[rowId];
-      }
-
-     // debugger
-      return { dataBlob, sectionIds, rowIds };
-
-  }
-
 
   _onShowModalPicker (name, currentColor) {
       const newdata = this.state.data.map(element => {
@@ -66,24 +22,51 @@ export default class App extends React.Component {
             return element
         });
       this.setState({
-            dataSource: this.state.dataSource.cloneWithRowsAndSections(newdata),
+            dataSource: this.state.dataSource.cloneWithRows(newdata),
             data: newdata
         });
 
   }
 
   renderRow(data) {
-      console.log(data)
       return (<Row data={data} key={data.name} onButtonPress={this._onShowModalPicker.bind(this)}/>)
   }
 
-  renderHeader() {
-      return (<View style={styles.container}>
-          <Text style={styles.input}>
-              {'Test this'}
-          </Text>
+  _onSaveButton = () => {
+      alert('Save Clicked');
+    }
 
-      </View>)
+    _onCancelButton = () => {
+        alert('Cancel Clicked');
+      }
+
+  renderHeader() {
+      return (
+          <View style={styles.header}>
+              <TouchableNativeFeedback
+                  onPress={this._onCancelButton}
+                  background={TouchableNativeFeedback.SelectableBackgroundBorderless()}>
+                  <View style={{padding:16}}>
+                      <Text style={[styles.input]}>
+                          {'Cancel'}
+                      </Text>
+                  </View>
+              </TouchableNativeFeedback>
+              <Text style={[styles.input, {fontSize:18}, {fontWeight: 'bold'}]}>
+                  {'ThemePicker'}
+              </Text>
+              <TouchableNativeFeedback
+                  onPress={this._onSaveButton}
+                  background={TouchableNativeFeedback.SelectableBackgroundBorderless()}>
+                  <View style={{padding:16}}>
+                      <Text style={[styles.input]}>
+                          {'Save'}
+                      </Text>
+                  </View>
+              </TouchableNativeFeedback>
+
+          </View>
+      )
   }
 
   render() {
@@ -93,21 +76,32 @@ export default class App extends React.Component {
             dataSource={this.state.dataSource}
             renderRow={this.renderRow.bind(this)}
             renderSeparator={(sectionId, rowId) => <View key={rowId} style={styles.separator} />}
-            renderSectionHeader={this.renderHeader}
-            stickySectionHeadersEnabled={true}
+            renderHeader={this.renderHeader.bind(this)}
+            renderFooter={this.renderHeader.bind(this)}
         />
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: 20,
-  },
-  separator: {
-    flex: 1,
-    height: StyleSheet.hairlineWidth,
-    backgroundColor: '#8E8E8E',
-  }
+    header: {
+        flex: 1,
+        height: 56,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: '#E6E6EB'
+    },
+    input: {
+        fontSize:16,
+    },
+    container: {
+        flex: 1,
+        marginTop: 20,
+    },
+    separator: {
+        flex: 1,
+        height: StyleSheet.hairlineWidth,
+        backgroundColor: '#8E8E8E',
+    }
 });
