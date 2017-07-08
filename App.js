9a33672bc48a3568e4095cf6ff0abd430049b7b1
Amit <ahetawal@salesforce.com>
2017-07-07 17:12:09 -0700

    WIP

--- App.js
+++ App.js
@@ -3,16 +3,57 @@ import { StyleSheet, Text, View, ListView, Modal } from 'react-native';
 import mockData from './mockData'
 import Row from './PickerRow'
 
+
+
 export default class App extends React.Component {
     constructor(props) {
     super(props);
 
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
+
+    const getSectionData = (dataBlob, sectionId) => dataBlob[sectionId];
+    const getRowData = (dataBlob, sectionId, rowId) => dataBlob[`${rowId}`];
+
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2,
+      sectionHeaderHasChanged : (s1, s2) => s1 !== s2,
+      getSectionData,
+      getRowData,
+    });
+
+    const { dataBlob, sectionIds, rowIds } = this.formatData(mockData);
+
     this.state = {
-      dataSource: ds.cloneWithRows(mockData),
+      dataSource: ds.cloneWithRowsAndSections(dataBlob, sectionIds, rowIds),
       data: mockData
     };
   }
+
+
+  formatData(data) {
+      const dataBlob = {};
+      const sectionIds = [];
+      const rowIds = [];
+      rowIds.push([]);
+
+      sectionIds.push(0);
+      dataBlob[0] = 'Header';
+
+    //   for (let sectionId = 0; sectionId < data.length; sectionId++) {
+    //       sectionIds.push(sectionId);
+    //       dataBlob[sectionId] = 'Header';
+    //   }
+      for (let rowId = 0; rowId < data.length; rowId++) {
+          const rowIdTest = `0:${rowId}`;
+         rowIds[0].push(rowIdTest);
+         dataBlob[rowIdTest] = data[rowId];
+      }
+
+     // debugger
+      return { dataBlob, sectionIds, rowIds };
+
+  }
+
 
   _onShowModalPicker (name, currentColor) {
       const newdata = this.state.data.map(element => {
@@ -22,7 +63,7 @@ export default class App extends React.Component {
             return element
         });
       this.setState({
-            dataSource: this.state.dataSource.cloneWithRows(newdata),
+            dataSource: this.state.dataSource.cloneWithRowsAndSections(newdata),
             data: newdata
         });
 
@@ -48,7 +89,8 @@ export default class App extends React.Component {
             dataSource={this.state.dataSource}
             renderRow={this.renderRow.bind(this)}
             renderSeparator={(sectionId, rowId) => <View key={rowId} style={styles.separator} />}
-            renderHeader={() => this.renderHeader()}
+            renderSectionHeader={this.renderHeader}
+            stickySectionHeadersEnabled={true}
         />
     );
   }
